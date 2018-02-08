952660bc980a565a22bc1583ecd45be2fb1c5868
Ben Forshey <hello@integrisweb.com>
2018-02-08 09:47:12 -0500

    initial commit after removing stale content

---
+++ js/app.js
@@ -0,0 +1,7 @@
+// Foundation JavaScript
+// Documentation can be found at: http://foundation.zurb.com/docs
+
+// @codekit-prepend "../bower_components/foundation/js/vendor/jquery.js"
+// @codekit-prepend "../bower_components/foundation/js/foundation.js"
+
+$(document).foundation();
